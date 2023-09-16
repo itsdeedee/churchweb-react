dd2c1a93c47d0fea5d26cc078489ae62794b4be5
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-09-16 23:53:44 +0100

    Update Ministries.tsx

--- src/pages/Ministries.tsx
+++ src/pages/Ministries.tsx
@@ -1,10 +1,17 @@
 import Header from "../components/Header.tsx";
+import "../Ministries.css";
 
 export default function Ministries() {
   return (
     <>
       <Header />
-      <h2>Ministries Page</h2>
+      <div className="Head About-head">
+        <h2>MINISTRIES</h2>
+        <p></p>
+      </div>
+      <div className="ministry-content">
+        <h2>MEET OUR MINISTRIES AND DEPARTMENTS</h2>
+      </div>
     </>
   );
 }

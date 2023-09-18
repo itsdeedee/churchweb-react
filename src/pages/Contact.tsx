683b7d9de7b385537a8ff2c5444ee53e1b452423
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-09-18 22:52:02 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,84 @@
 import Header from "../components/Header.tsx";
+import "../Contact.css";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer.tsx";
 
 export default function Contact() {
   return (
     <>
       <Header />
-      <h2>Contact Page</h2>
+      <div className="Head About-head">
+        <h2>CONTACT US</h2>
+        <p></p>
+      </div>
+      <div className="Contact1">
+        <div className="Contact-content1">
+          <h2>Write Us a Message</h2>
+          <p></p>
+          <button className="send-B">
+            Send{" "}
+            <i
+              className="fa-regular fa-paper-plane"
+              style={{ color: "#ffffff" }}
+            ></i>
+          </button>
+        </div>
+        <div className="contact-us">
+          {" "}
+          <div className="socials2">
+            <h2>
+              <i className="fa-solid fa-share-nodes" />
+              <span>SOCIAL MEDIA</span>
+            </h2>
+            <div className="c-icon">
+              <Link to="/">
+                <i
+                  className="fa-brands fa-facebook"
+                  style={{ color: " #251C46", fontSize: "28px" }}
+                ></i>
+              </Link>
+              <Link to="/">
+                <i
+                  className="fa-brands fa-twitter"
+                  style={{
+                    color: " #251C46",
+                    fontSize: "28px",
+                    marginLeft: "30px",
+                  }}
+                ></i>
+              </Link>
+            </div>
+          </div>
+          <div className="mail">
+            <h2>
+              <i className="fa-regular fa-envelope" /> <span>EMAIL</span>
+            </h2>
+            <Link to="mailto:admin@rccghom.org" className="mail-link">
+              admin@rccghom.org
+            </Link>
+          </div>
+          <div className="phone">
+            <h2>
+              {" "}
+              <i className="fa-solid fa-phone" />
+              {"     "}
+              <span>PHONE</span>
+            </h2>
+            <p>07421369153</p>
+          </div>
+        </div>
+      </div>
+      <div className="Contact2">
+        <h2>JOIN US FOR SERVICE</h2>
+        <p>SUNDAYS- 10:30 -1:00</p>
+        <div className="contact-banner"></div>
+        <p>
+          {" "}
+          <i className="fa-solid fa-location-dot" /> 6 Alfall Rd, Coventry CV2
+          3GB
+        </p>
+      </div>
+      <Footer />
     </>
   );
 }

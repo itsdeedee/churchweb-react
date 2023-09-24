bebd940e2ac7b4bc8526c05afd1849ced098ab9e
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-09-24 14:55:39 +0100

    Create ContactForm.tsx

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,72 @@
+import emailjs from "emailjs-com";
+import { Form, Input, TextArea, Button } from "semantic-ui-react";
+import Swal from "sweetalert2";
+import "../ContactForm.css";
+
+const SERVICE_ID = "service_omsi1nn";
+const TEMPLATE_ID = "template_fetr1xg";
+const USER_ID = "TlyDtMBamonx8DfSQ";
+
+const ContactForm = () => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong",
+          text: error.text,
+        });
+      }
+    );
+    e.target.reset();
+  };
+
+  return (
+    <div className="ContactForm">
+      <Form id="form" onSubmit={handleOnSubmit}>
+        <Form.Field
+          id="form-input-control-email"
+          control={Input}
+          label="Email"
+          name="user_email"
+          placeholder="Email…"
+          required
+          icon="mail"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-input-control-last-name"
+          control={Input}
+          label="Name"
+          name="user_name"
+          placeholder="Name…"
+          required
+          icon="user circle"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-textarea-control-opinion"
+          control={TextArea}
+          name="user_message"
+          placeholder="Message…"
+          required
+        />
+        <Button type="submit" id="submit">
+          Send{" "}
+          <i className="fa-solid fa-paper-plane" style={{ color: "#ffffff" }} />
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default ContactForm;

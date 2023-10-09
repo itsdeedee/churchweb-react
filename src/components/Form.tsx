090244a5c0c8c7c770030e31683ef9a5dbbd11a5
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-10-09 21:19:22 +0100

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,10 +9,14 @@ const NewsletterForm = () => {
           <Form className="w-64 p-4 flex flex-col">
             <h2>Sign up for our Newsletter!</h2>
             <div className="my-4 flex flex-col">
-              <Field name="Name" className="Name-field" />
+              <Field name="Name" className="Name-field" placeholder="Name..." />
             </div>
             <div className="my-4 flex flex-col">
-              <Field name="email" className="Name-field" />
+              <Field
+                name="email"
+                className="Name-field"
+                placeholder="Email..."
+              />
             </div>
             <button type="submit" className="Submit-button">
               SIGN UP

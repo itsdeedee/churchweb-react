21a1cd230e84af0e84b7dbc95dba98cfe936c859
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-10-10 21:00:55 +0100

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -19,7 +19,7 @@ export default function Gallery() {
     <>
       <Herocomponent header="Gallery" />
       <div className="gallery-container">
-        <div className="colum">
+        <div className="column">
           <div className="photo">
             <img src={photo1} alt="photo" />
           </div>

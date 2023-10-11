b1299ca24cfe1276a30e06e9ff7f57e2a967dfd5
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-10-11 22:24:02 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             className="about-content-link"
             style={{ textDecoration: "none" }}
           >
-            Learn Mor
+            Learn More
           </Link>
         </div>
       </div>
